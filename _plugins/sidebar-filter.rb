474bd21ed649aaa5ded0bf8650c90f728e31dcc7
Oscar M.A <omaok.11leon@gmail.com>
2025-09-28 22:20:51 -0500

    Fix sidebar-filter plugin: handle nil current_page safely

--- _plugins/sidebar-filter.rb
+++ _plugins/sidebar-filter.rb
@@ -1,5 +1,7 @@
 Jekyll::Hooks.register :site, :post_read do |site|
   # Filter tabs by language for the sidebar
+  return unless site.collections['tabs']
+  
   site.collections['tabs'].docs.each do |tab|
     # Set default language if not specified
     tab.data['lang'] ||= 'en'
@@ -7,16 +9,30 @@ Jekyll::Hooks.register :site, :post_read do |site|
 end
 
 Jekyll::Hooks.register :site, :pre_render do |site, payload|
+  # Skip if tabs collection doesn't exist
+  return unless site.collections['tabs']
+  
   # Filter tabs based on current page language
   current_page = payload['page']
-  current_lang = current_page['lang'] || 'en'
+  
+  # Safely get current language with multiple fallbacks
+  current_lang = case
+                 when current_page && current_page['lang']
+                   current_page['lang']
+                 when payload['site'] && payload['site']['lang']
+                   payload['site']['lang']
+                 when site.config['lang']
+                   site.config['lang']
+                 else
+                   'en'
+                 end
   
   # Filter tabs collection to show only tabs matching current language
   filtered_tabs = site.collections['tabs'].docs.select do |tab|
-    tab.data['lang'] == current_lang
+    (tab.data['lang'] || 'en') == current_lang
   end
   
-  # Sort by order
+  # Sort by order if order is specified
   filtered_tabs.sort_by! { |tab| tab.data['order'] || 999 }
   
   # Replace the tabs collection with filtered tabs
